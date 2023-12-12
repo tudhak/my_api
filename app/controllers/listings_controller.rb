5b1c3085daaf413ba3a1cf95bcf48295bd503316
Tudal Haccandy <tud.hak_alt@outlook.com>
2023-12-13 00:24:01 +0100

    controller methods

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,5 +1,5 @@
 class ListingsController < ApplicationController
-  before_action :set_listing, only: [:show]
+  before_action :set_listing, only: [:show, :destroy]
 
   def index
     @listings = Listing.all
@@ -9,9 +9,36 @@ class ListingsController < ApplicationController
   def show
   end
 
+  def create
+    @listing = Listing.new(listing_params)
+    if @listing.save
+      redirect_to listing_path(@listing)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @listing.update(listing_params)
+      redirect_to listing_path(@listing)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @listing.destroy
+    redirect_to listing_path(@listing), status: :see_other
+  end
+
+
   private
 
   def set_listing
     @listing = Listing.find(params[:id])
+  end
+
+  def listing_params
+    params.require(:listing).permit(:num_rooms)
   end
 end
